4086629e315fb2e17bd66b03410290fc65139d7c
arysteasz <arysteasz@gmail.com>
2020-05-13 19:46:03 +0200

    Plik dukujacy Imie i Nazwisko

---
+++ Wizytowka.py
@@ -0,0 +1,15 @@
+nazwisko=input("jak się nazywasz: ")
+tytul= "Developer"
+dl_nazwiska = len(nazwisko)
+dl_tytulu = len(tytul)
+roznica = abs(dl_nazwiska - dl_tytulu)
+left_padding = roznica //2
+right_padding = roznica - left_padding
+
+print(f"Nazwisko: {nazwisko}")
+print('+-'+'-' *dl_nazwiska + '-+')
+print('| ' + nazwisko + ' |')
+print('| '+ ' '* left_padding + tytul + ' '*right_padding +' |')
+print('+-'+'-' *dl_nazwiska + '-+')
+
+
