c8e68804adf1e6df36d5e7b3d2ab1ea139f51f7c
arysteasz <arysteasz@gmail.com>
2020-05-13 20:14:07 +0200

    Program do losowania losowej liczby stałoprzecinkowej

---
+++ lotoo.py
@@ -0,0 +1,16 @@
+import random
+
+liczba = random.randint(1, 10)
+print("Wylosowana liczba ", liczba)
+
+for i in range(3):
+    odp = int(input("Jaką liczbę od 1 do 10 mam na myśli? "))
+    # print("Podałeś liczbę: ", odp)
+
+    if liczba == int(odp):
+        print("Zgadłeś! Dostajesz długopis!")
+        break
+    else:
+        print("Nie zgadłeś. Spróbuj jeszcze raz.")
+        print()
+
